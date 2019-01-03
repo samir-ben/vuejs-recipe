0349489a92fc57033aabeab32e969126616831f8
samir-ben <samir-69@hotmail.fr>
2019-01-04 00:49:30 +0100

    feat: edit route config
    - add an edit route with param in router config

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '@/components/Home'
 import AddRecipe from '@/components/AddRecipe'
+import EditRecipe from '@/components/EditRecipe';
 
 Vue.use(Router)
 
@@ -16,6 +17,11 @@ export default new Router({
       path: '/add-recipe',
       name: 'AddRecipe',
       component: AddRecipe
+    },
+    {
+      path: '/edit-recipe/:id',
+      name: 'EditRecipe',
+      component: EditRecipe
     }
   ]
-})
+});
