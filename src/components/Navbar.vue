cd74a78494da6ff6a416b6827c9f8220f0c170fa
samir-ben <samir-69@hotmail.fr>
2019-01-03 21:05:44 +0100

    feat: navbar links
    - add dynamic links in navbar component

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,11 +1,11 @@
 <template>
     <nav class="ui container">
         <div class="ui container top attached tabular mini menu">
-                <router-link to="/" class="active item">Home</router-link>
+                <router-link :to="{ name: 'Home'}" class="active item">Home</router-link>
             <div class="right menu">
                 <div class="item">
                     <div class="ui icon compact mini menu">
-                        <router-link to="/" class="item">
+                        <router-link :to="{ name: 'AddRecipe'}" class="item">
                             <i class=" teal plus icon"></i>
                         </router-link>
                     </div>
