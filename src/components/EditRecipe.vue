54b16e763c08038c03526946dd19c1f99756406b
samir-ben <samir-69@hotmail.fr>
2019-01-04 00:48:07 +0100

    feat: edit component
    - created edit component

---
+++ src/components/EditRecipe.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="ui container raised very padded segment">
+    <h2>Modifier la recette</h2>
+   
+  </div>
+</template>
+
+<script>
+import db from '@/firebase/init'
+
+
+export default {
+  name: "EditRecipe",
+  data() {
+    return {
+    };
+  },
+};
+</script>
+
+<style scoped>
+</style>
