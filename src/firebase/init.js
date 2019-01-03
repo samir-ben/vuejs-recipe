60bb285c2e591c52a0664dab3c52287c7338278a
samir-ben <samir-69@hotmail.fr>
2019-01-03 19:59:26 +0100

    config: firebase
    - add firebase npm package
    - created firebase folder with ini js file
    - add firebase configuration in ini js file
    - other, add jquery in index html

---
+++ src/firebase/init.js
@@ -0,0 +1,17 @@
+import firebase from 'firebase'
+import 'firebase/firestore';
+
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyDqRTORT57jk4ULcT4oE1iCHblOyvdrsP0",
+    authDomain: "recipes-vue.firebaseapp.com",
+    databaseURL: "https://recipes-vue.firebaseio.com",
+    projectId: "recipes-vue",
+    storageBucket: "recipes-vue.appspot.com",
+    messagingSenderId: "192281069452"
+};
+
+const firebaseApp = firebase.initializeApp(config);
+firebaseApp.firestore().settings({ timestampsInSnapshots: true });
+
+export default firebaseApp.firestore();
