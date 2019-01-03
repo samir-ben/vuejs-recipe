6057092eaea347277ac878b7bcea896eb469aad6
samir-ben <samir-69@hotmail.fr>
2019-01-03 18:18:27 +0100

    feat: navbar & home component
    - reset all app
    - delete helloworld componnet
    - add navbar and home component
    - add home component in router file
    - add ui menu in navbar component
    - add navbar component in app vue

---
+++ src/components/Home.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <div class="ui container">
+            <h1>Home</h1>
+        </div>
+    </div>
+</template>
+
+
+<script>
+export default {
+   name: 'Navbar',
+   data(){
+    return {
+      
+    }
+  } 
+}
+</script>
+
+
+
+<style scoped>
+
+</style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,36 @@
+<template>
+    <nav class="ui container">
+        <div class="ui container top attached tabular mini menu">
+                <router-link to="/" class="active item">Home</router-link>
+            <div class="right menu">
+                <div class="item">
+                    <div class="ui icon compact mini menu">
+                        <router-link to="/" class="item">
+                            <i class=" teal plus icon"></i>
+                        </router-link>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </nav> 
+</template>
+
+
+<script>
+export default {
+   name: 'Navbar',
+   data(){
+    return {
+      
+    }
+  } 
+}
+</script>
+
+
+
+<style scoped>
+
+</style>
+
+
