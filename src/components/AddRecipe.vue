ba898eb5c723813c5b54da513efb36ac017bb233
samir-ben <samir-69@hotmail.fr>
2019-01-03 21:18:06 +0100

    feat: form
    - add semantic form template

--- src/components/AddRecipe.vue
+++ src/components/AddRecipe.vue
@@ -1,20 +1,36 @@
 <template>
-    <div>
-        hello
-    </div>
+  <div class="ui container raised very padded segment">
+    <h2>Ajouter une nouvelle recette</h2>  
+    <form class="ui form" @submit.prevent="AddRecipe">
+      <div class="field">
+        <label for="title">Titre</label>
+        <input name="title" type="text" v-model="title">
+      </div>
+      <div class="field">
+        <label for="ingredient">Ingrédient</label>
+        <input name="ingredient" type="text">
+      </div>
+      <button class="ui submit button">Ajouter</button>
+      <button class="ui error message"></button>
+    </form>
+  </div>
 </template>
 
 <script>
 export default {
-    name: 'AddRecipe',
-    data() {
-        return {
-
-        }
-    }
-}
+  name: "AddRecipe",
+  data() {
+    return {
+        title: null
+    };
+  },
+  methods: {
+      AddRecipe() {
+          console.log(this.title)
+      }
+  },
+};
 </script>
 
 <style scoped>
-
 </style>
