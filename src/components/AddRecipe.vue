32e1d462e869cbf4b57ac2083a95ded05ee08ce5
samir-ben <samir-69@hotmail.fr>
2019-01-03 21:02:04 +0100

    feat: add recipe component
    - created ass recipe component empty

---
+++ src/components/AddRecipe.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        hello
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'AddRecipe',
+    data() {
+        return {
+
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
