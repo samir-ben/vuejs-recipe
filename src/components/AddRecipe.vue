9de3c035db3d72ec4f790cb6b459cc1bdd3618da
samir-ben <samir-69@hotmail.fr>
2019-01-03 22:08:19 +0100

    feat: input ingredient
    - add input ingredient for each data when user clics tab
    - add message error when ingredient input is empty

--- src/components/AddRecipe.vue
+++ src/components/AddRecipe.vue
@@ -1,17 +1,23 @@
 <template>
   <div class="ui container raised very padded segment">
-    <h2>Ajouter une nouvelle recette</h2>  
+    <h2>Ajouter une nouvelle recette</h2>
     <form class="ui form" @submit.prevent="AddRecipe">
       <div class="field">
         <label for="title">Titre</label>
         <input name="title" type="text" v-model="title">
       </div>
+      <div class="field" v-for="(ing, index) in ingredients" :key="index">
+        <label for="ingredients">Ingrédient</label>
+        <input name="ingredients" type="text" v-model="ingredients[index]">
+      </div>
       <div class="field">
-        <label for="ingredient">Ingrédient</label>
-        <input name="ingredient" type="text">
+        <label for="ingredient">Ajouter un ingrédient</label>
+        <input name="ingredient" type="text" @keydown.tab.prevent="addIng" v-model="another">
+      </div>
+      <div class="field">
+        <p class="ui yellow message" v-if="feedback">{{feedback}}</p>
       </div>
       <button class="ui submit button">Ajouter</button>
-      <button class="ui error message"></button>
     </form>
   </div>
 </template>
@@ -21,14 +27,26 @@ export default {
   name: "AddRecipe",
   data() {
     return {
-        title: null
+      title: null,
+      another: null,
+      ingredients: [],
+      feedback: null
     };
   },
   methods: {
-      AddRecipe() {
-          console.log(this.title)
+    AddRecipe() {
+      console.log(this.title, this.ingredients);
+    },
+    addIng() {
+      if (this.another) {
+        this.ingredients.push(this.another);
+        this.another = null;
+        this.feedback= null;
+      } else {
+        this.feedback = "Veuillez ajouter un ingrédient";
       }
-  },
+    }
+  }
 };
 </script>
 
