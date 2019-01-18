721e2709ed6f88865710d316190cc101180819d2
samir-ben <33352678+samir-ben@users.noreply.github.com>
2019-01-19 00:59:55 +0100

    Update init.js

--- src/firebase/init.js
+++ src/firebase/init.js
@@ -3,12 +3,7 @@ import 'firebase/firestore';
 
 // Initialize Firebase
 var config = {
-    apiKey: "AIzaSyDqRTORT57jk4ULcT4oE1iCHblOyvdrsP0",
-    authDomain: "recipes-vue.firebaseapp.com",
-    databaseURL: "https://recipes-vue.firebaseio.com",
-    projectId: "recipes-vue",
-    storageBucket: "recipes-vue.appspot.com",
-    messagingSenderId: "192281069452"
+    //YOUR FIREBASE CONFIG
 };
 
 const firebaseApp = firebase.initializeApp(config);
